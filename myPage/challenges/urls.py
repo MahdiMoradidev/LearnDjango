927c7a68b4c9802665ba012a2eef1cea4582ecf6
Mahdi Moradi <alakialakiani@yahoo.com>
2021-11-14 20:48:42 +0330

    Add number url support for month

--- myPage/challenges/urls.py
+++ myPage/challenges/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index),
-    path("january", views.january),
-    path("february", views.february)
+    path("<int:month>", views.monthly_challenge_by_number),
+    path("<str:month>", views.monthly_challenge)
 ]
--- myPage/challenges/views.py
+++ myPage/challenges/views.py
@@ -1,16 +1,26 @@
 from django.shortcuts import render
-from django.http import response
+from django.http import HttpResponse, HttpResponseNotFound
 
 # Create your views here.
 
 
 def index(request):
-    return response.HttpResponse("This is Index page")
+    return HttpResponse("This is Index page")
 
 
-def january(request):
-    return response.HttpResponse("This is January page")
+def monthly_challenge_by_number(request, month):
+    return HttpResponse(month)
 
 
-def february(request):
-    return response.HttpResponse("February works good!")
+def monthly_challenge(request, month):
+    challeng_text = None
+
+    if month == "january":
+        challeng_text = "This is January page"
+    elif month == "february":
+        challeng_text = "This is February page"
+    elif month == "march":
+        challeng_text = "This is March page"
+    else:
+        return HttpResponseNotFound("<h1>This month is not supported!</h1>")
+    return HttpResponse(challeng_text)
