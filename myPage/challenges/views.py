6daf659a130bcc4de98860416fbade95242e9977
Mahdi Moradi <alakialakiani@yahoo.com>
2021-11-14 19:52:48 +0330

    Add Jan and Feb actions

--- myPage/challenges/views.py
+++ myPage/challenges/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import response
 
 # Create your views here.
+
+
+def index(request):
+    return response.HttpResponse("This is Index page")
+
+
+def january(request):
+    return response.HttpResponse("This is January page")
+
+
+def february(request):
+    return response.HttpResponse("February works good!")
