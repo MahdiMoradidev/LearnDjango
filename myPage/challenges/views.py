5d64cbd98a7b7159cfd90b3e04c19b0cadb54d7b
Mahdi Moradi <alakialakiani@yahoo.com>
2021-11-15 19:13:34 +0330

    Redirect done with another way

--- myPage/challenges/views.py
+++ myPage/challenges/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 
 # Create your views here.
+challenges = {
+    "january": "This is January page",
+    "february": "This is February page",
+    "march": "This is March page",
+    "april": "This is April page",
+    "may": "This is May page",
+    "june": "This is June page",
+    "july": "This is July page",
+    "august": "This is August page",
+    "september": "This is September page",
+    "october": "This is October page",
+    "november": "This is November page",
+    "december": "This is December page"
+}
 
 
 def index(request):
@@ -9,18 +23,31 @@ def index(request):
 
 
 def monthly_challenge_by_number(request, month):
-    return HttpResponse(month)
+    months = list(challenges.keys())
+
+    if month < 1 or month > len(months):
+        return HttpResponseNotFound("<h1>" + "Out of value range!" + "</h1>")
+
+    redirect_month = months[month - 1]
+    return HttpResponseRedirect("/challenges/" + redirect_month)
 
 
 def monthly_challenge(request, month):
-    challeng_text = None
+    try:
+        challenges_text = challenges[month]
+        return HttpResponse("<h1>" + challenges_text + "<h1>")
+    except:
+        HttpResponseNotFound("This month is not suppurted!")
 
-    if month == "january":
-        challeng_text = "This is January page"
-    elif month == "february":
-        challeng_text = "This is February page"
-    elif month == "march":
-        challeng_text = "This is March page"
-    else:
-        return HttpResponseNotFound("<h1>This month is not supported!</h1>")
-    return HttpResponse(challeng_text)
+    # try:
+    #     if month.isdigit() and (0 < int(month) < 13):
+    #         challeng_text = list(challenges.values())
+    #         return HttpResponse("<h1>" + challeng_text[(int(month)-1)] + "</h1>")
+    #         # monthly_challenge(None, str(list(challenges)[month]))
+    #     elif month.isdigit() and (12 < int(month) < 1):
+    #         return HttpResponseNotFound("<h1>" + "This number is not supported!" + "</h1>")
+    #     else:
+    #         challeng_text = challenges[month]
+    #         return HttpResponse("<h1>" + challeng_text + "</h1>")
+    # except:
+    #     return HttpResponseNotFound("<h1>" + "This value is not supported!" + "</h1>")
